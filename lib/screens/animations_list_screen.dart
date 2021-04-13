a25519ad52ad3ada7cfae230c67a49514d6582f3
salih <salihcn.bnb@gmail.com>
2021-04-13 05:40:33 +0300

    Add implicity screens

--- lib/screens/animations_list_screen.dart
+++ lib/screens/animations_list_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'implicit_screens/animated_container_with_curves_screen.dart';
+import 'implicit_screens/animated_opacity_screen.dart';
+
 class AnimationsListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -8,10 +11,19 @@ class AnimationsListScreen extends StatelessWidget {
       body: Builder(
         builder: (context) => ListView(
           children: [
-            ListTile(title: Text('Implicit -> AnimatedOpacity')),
-            ListTile(title: Text('Implicit -> AnimatedSize')),
+            ListTile(
+              title: Text('Implicit -> AnimatedOpacity'),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (ctx) => AnimatedOpacityScreen()));
+              },
+            ),
+            ListTile(
+              title: Text('Implicit -> AnimatedContainer with Curves'),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (ctx) => AnimatedContainerWithCurvesScreen()));
+              },
+            ),
             ListTile(title: Text('Implicit -> TweenAnimationBuilder')),
-            ListTile(title: Text('Implicit -> with Curves')),
           ],
         ),
       ),
